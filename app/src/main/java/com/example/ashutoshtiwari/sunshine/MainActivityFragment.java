c6f1e9085436c2574b6e1bef2d33f427a17866ef
Ashutosh Tiwari <Ashutosh.tiwari@Simform-135.local>
2017-06-14 17:25:55 +0530

    first commit

---
+++ app/src/main/java/com/example/ashutoshtiwari/sunshine/MainActivityFragment.java
@@ -0,0 +1,15 @@
+package com.example.ashutoshtiwari.sunshine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_main);
+
+
+    }
+}
