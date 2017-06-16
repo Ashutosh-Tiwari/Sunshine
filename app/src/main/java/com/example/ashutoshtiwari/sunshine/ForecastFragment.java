d1629b13513071596e1208521f53fc8afcb4810e
Ashutosh.tiwari <ashutiwari9518@gmail.com>
2017-06-16 19:24:20 +0530

    ADDED: REFRESH ACTION AS POPUP MENU INTERNET PERMISSION API KEY IN GRADLE.PROPERTIES AsyncTask FOR FETCHING WEATHER DATA (Incomplete)

--- app/src/main/java/com/example/ashutoshtiwari/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/ashutoshtiwari/sunshine/ForecastFragment.java
@@ -1,9 +1,13 @@
 package com.example.ashutoshtiwari.sunshine;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -28,12 +32,29 @@ public class ForecastFragment extends Fragment {
     ListView forecastList;
     ArrayAdapter<String> mForecastAdapter;
 
-    HttpURLConnection urlConnection = null;
-    BufferedReader reader = null;
-
-    String forecastJsonString = null;
 
     public ForecastFragment() {
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecast_fragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_refresh) {
+            new FetchWeatherTask().execute();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -43,7 +64,7 @@ public class ForecastFragment extends Fragment {
 
         String forecastArray[] = {
                 "Today - 88/63",
-                "Tomorrow -66/76",
+                "Tomorrow - 66/76",
                 "Wednesday - 55/65",
                 "Thursday - 87/67",
                 "Friday - 78/67",
@@ -57,47 +78,65 @@ public class ForecastFragment extends Fragment {
         forecastList = (ListView) rootView.findViewById(R.id.listview_forecast);
         forecastList.setAdapter(mForecastAdapter);
 
-        try {
-            URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?zip=94043,us&mode=json&units=metric&cnt=7");
-
-            //Create connection to openWeatherMap query
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.connect();
-
-            //Read inputstream into a string
-            InputStream inputStream = urlConnection.getInputStream();
-            StringBuffer buffer = new StringBuffer();
-            if (inputStream == null) {
-                return null;
-            }
-            reader = new BufferedReader(new InputStreamReader(inputStream));
-
-            String line;
-
-            while ((line = reader.readLine()) != null) {
-                buffer.append(line).append("\n");
-            }
-
-            if (buffer.length() == 0) {
-                return null;
-            }
-
-            forecastJsonString = buffer.toString();
-
-        } catch (IOException e) {
-            Log.e("ForecastFragment", "Error", e);
-            return null;
-        } finally {
-            if (urlConnection != null)
-                urlConnection.disconnect();
-            if (reader != null)
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    Log.e("ForecastFragment", "Error closing reader", e);
-                }
-        }
         return rootView;
+    }
+
+    public class FetchWeatherTask extends AsyncTask<Void, Void, Void> {
+
+        private final String TAG = FetchWeatherTask.class.getSimpleName();
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            String forecastJsonString = null;
+
+            try {
+                String baseUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=94043,us&mode=json&units=metric&cnt=7";
+                String apiKey = "&APIID=" + BuildConfig.OPEN_WEATHER_MAP_API_KEY;
+                URL url = new URL(baseUrl.concat(apiKey));
+
+                //Create connection to openWeatherMap query
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                //Read inputstream into a string
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line).append("\n");
+                }
+
+                if (buffer.length() == 0) {
+                    return null;
+                }
+
+                forecastJsonString = buffer.toString();
+                Log.e(TAG,"Forecast Json Data: " + forecastJsonString);
+
+            } catch (IOException e) {
+                Log.e(TAG, "Error", e);
+                return null;
+            } finally {
+                if (urlConnection != null)
+                    urlConnection.disconnect();
+                if (reader != null)
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        Log.e(TAG, "Error closing reader", e);
+                    }
+            }
+            return null;
+        }
     }
 }
